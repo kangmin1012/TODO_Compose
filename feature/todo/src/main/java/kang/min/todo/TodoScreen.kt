c1c57eed260b8625ab209381ae9012cdb77ef652
kangmin1012 <kangmin1012@naver.com>
2024-08-03 18:14:53 +0900

    Todo 카드 작성

--- feature/todo/src/main/java/kang/min/todo/TodoScreen.kt
+++ feature/todo/src/main/java/kang/min/todo/TodoScreen.kt
@@ -1,13 +1,13 @@
 package kang.min.todo
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import kang.min.todo.ui.TodoCheckBox
 import kang.min.ui.theme.TodoTheme
 
 @Composable
 fun TodoScreen() {
-    Text(text = "hello")
+    TodoCheckBox()
 }
 
 @Preview(showBackground = true)
---
+++ feature/todo/src/main/java/kang/min/todo/ui/TodoCheckBox.kt
@@ -0,0 +1,59 @@
+package kang.min.todo.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.toggleableState
+import androidx.compose.ui.state.ToggleableState
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kang.min.ui.theme.TodoTheme
+
+@Composable
+internal fun TodoCheckBox() {
+
+    val (checked, setChecked) = remember{ mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(16.dp)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                setChecked(!checked)
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            modifier = Modifier,
+            checked = checked,
+            onCheckedChange = { setChecked(it) }
+        )
+        Text(
+            text = "card"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TodoCheckBoxPreview() {
+    TodoTheme {
+        TodoCheckBox()
+    }
+}
